1322bba26e15ec9a8080fc3516ca3bbb7c66bcf9
John Andrew Registos <118709598+johnandrewregistos@users.noreply.github.com>
2023-05-10 12:42:10 +0800

    Update Cart.jsx
    add: Update Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -57,7 +57,7 @@ const Cart = () => {
 
   return (
     <div className="container mx-auto mt-10">
-      <div className="flex shadow-md my-10">
+      <div className="flex flex-col md:flex-row shadow-md my-10">
         <div className="w-3/4 bg-white px-10 py-10">
           <div className="flex justify-between border-b pb-8">
             <h1 className="font-semibold text-2xl">Shopping Cart</h1>
